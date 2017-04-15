91896f225dd866b120d7168b2f7801fcc4064913
Odet Alexandre <odet.alexandre.93@gmail.com>
2017-04-15 14:21:16 -0400

    HomeViewController: AddHeaderView()

--- Resume/Controllers/HomeViewController.swift
+++ Resume/Controllers/HomeViewController.swift
@@ -43,8 +43,34 @@ class HomeViewController: UIViewController {
     profileImage.translatesAutoresizingMaskIntoConstraints = false
     profileImage.image = R.image.profile()
     profileImage.round()
+    
+    addHeaderView()
   }
-
+  
+  //offset = (80 - (self.navigationController.navigationBar.frame.height + UIApplication.shared.statusBar.frame.height))
+  
+  private func addHeaderView() {
+    let headerView = UIView()
+    self.view.addSubview(headerView)
+    headerView.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(profileImage.snp.bottom).offset((80 - ((self.navigationController?.navigationBar.frame.height)! + UIApplication.shared.statusBarFrame.height)))
+      make.width.equalTo(self.view)
+      make.bottom.equalTo(self.view)
+    }
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+    headerView.backgroundColor = UIColor.white
+    
+    let nameLabel = UILabel()
+    headerView.addSubview(nameLabel)
+    nameLabel.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(headerView).offset(5)
+      make.leading.equalTo(headerView).offset(10)
+    }
+    nameLabel.translatesAutoresizingMaskIntoConstraints = false
+    nameLabel.text = "Alexandre Odet"
+    
+  }
+  
   private func setUpNavigationBarButtons() {
     let rightButton = UIBarButtonItem(image: R.image.skills(),
                                       style: .plain, target: self,
@@ -64,7 +90,6 @@ class HomeViewController: UIViewController {
   func projectsButtonTarget() {
     let nextViewController = GithubProjectListTableViewController()
     self.navigationController?.pushViewController(nextViewController, animated: true)
-    print("Projects Button tapped")
   }
   
 }
